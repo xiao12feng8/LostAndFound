c680e7e1165480195cf917dfaeb5fc71c09fd28e
xiaofeng <xiao12feng8@example.com>
2025-03-25 17:40:52 +0800

    Initial commit

---
+++ src/main/java/com/example/lostandfound/controller/LostAndFoundController.java
@@ -0,0 +1,13 @@
+package com.example.lostandfound.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LostAndFoundController {
+
+    @GetMapping("/)
+    public String test() {
+        return "Hello world";
+    }
+}
